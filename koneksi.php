c6c557cb05ee25d2a73523782a06f715a2a4b128
firlyyyyy <firlyalfaridzi88@gmail.com>
2025-01-13 14:53:00 +0800

    done

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+    $hostname = 'phpmyadmin.test';
+    $username = 'firly';
+    $password = 'firly';
+    $database = 'pengaduan_masyarakat';
+
+    $koneksi = new mysqli($hostname, $username, $password, $database);
+?>
---
+++ register.php
@@ -0,0 +1,64 @@
+<?php
+    require 'koneksi.php';
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $nik = $_POST['nik'];
+
+        $sql = 'SELECT * FROM masyarakat WHERE nik=?';
+        $cek = $koneksi->execute_query($sql, [$nik]);
+
+        if (mysqli_num_rows($cek) == 1) {
+            echo "<script>alert('NIK sudah digunakan')</script>";
+        } else {
+            $nama = $_POST['nama'];
+            $telepon = $_POST['telp'];
+            $username = $_POST['username'];
+            $password = md5($_POST['password']);
+            $sql = 'INSERT INTO masyarakat SET nik=?, telp=?, username=?, password=?';
+            $koneksi->execute_query($sql, [$nik, $telepon, $username, $password]);
+            echo "<script>alert('Pendaftaran Berhasil')</script>";
+            header('location:login.php');
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrasi</title>
+</head>
+<body>
+    <h1>Registrasi Pengguna</h1>
+    <form action="" method="post">
+        <div class="form-item">
+            <label for="nik">NIK</label>
+            <input type="text" name="nik" id="nik">
+        </div>
+
+        <div class="form-item">
+            <label for="nama">Nama Lengkap</label>
+            <input type="text" name="nama" id="nama">
+        </div>
+
+        <div class="form-item">
+            <label for="telepon">Telepon</label>
+            <input type="tel" name="telepon" id="telepon">
+        </div>
+
+        <div class="form-item">
+            <label for="useername">Username</label>
+            <input type="text" name="useername" id="useername">
+        </div>
+
+        <div class="form-item">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+        </div>
+
+        <button type="submit">Register</button>
+    </form>
+    <a href="login.php">Batal</a>
+</body>
+</html>
